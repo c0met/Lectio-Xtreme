4b6f9db29e0348dd9ffa3e9e02bb9e25b7841334
Nidocq <phillip.linnemann.lundin@gmail.com>
2018-05-02 00:14:32 +0200

    Made a setup under the udvidelser (baggrundsside)

---
+++ background.js
@@ -0,0 +1,7 @@
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    var version = "v1.1"
+    console.log("Version of Lectio X is : " + version);
+  });
+});
